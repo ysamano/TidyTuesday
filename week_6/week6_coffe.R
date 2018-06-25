a64e841868e54a69c4aa7d821934adf853225b5e
Yobanny <yobanny_sc@hotmail.com>
2018-06-24 22:05:36 -0500

    script

---
+++ week_6/week6_coffe.R
@@ -0,0 +1,18 @@
+library(ggplot2)
+library(waffle)
+
+starbucks <- readxl::read_excel("week_6/week6_coffee_chains.xlsx", sheet = "starbucks")
+timhorton <- readxl::read_excel("week_6/week6_coffee_chains.xlsx", sheet = "timhorton")
+dunkin <- readxl::read_excel("week_6/week6_coffee_chains.xlsx", sheet = "dunkin")
+
+num_places <- c(Starbucks = nrow(starbucks),
+                `Tim Hortons` = nrow(timhorton),
+                `Dunkin Donuts\n(only USA)` = nrow(dunkin))
+
+coffe_plot <- waffle(num_places / 30,
+                     rows = 25,
+                     colors = c("#238b45", "#cb181d", "#dd3497"),
+                     legend_pos = "bottom", title = "Number of Cofee Stores in the World", 
+                     xlab = "1 square is 30 places")
+
+ggsave("week_6/coffe_plot.png", coffe_plot, height = 5, width = 8, units = "in", dpi = 500)
