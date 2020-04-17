78ede07aac763df1e0c523f09fb5ceb39ee65813
Yobanny <yobanny_sc@hotmail.com>
2020-04-16 21:30:28 -0500

    Commit

---
+++ 2020/week_16/week_16_hip_hop.R
@@ -0,0 +1,76 @@
+
+library(tidyverse)
+library(packcircles)
+library(patchwork)
+library(ggtext)
+extrafont::loadfonts(device = "win")
+
+rankings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-04-14/rankings.csv')
+
+rankings2 <- rankings %>%
+    select(ID, title, artist, year, points, gender) %>% 
+    mutate(colour = case_when(gender == "male" ~ "#0b53c1",
+                              gender == "female" ~ "#ff0055", 
+                              TRUE ~ "#ffec1b"))
+
+packing <- rankings2 %>% 
+    arrange(year) %>% 
+    circleProgressiveLayout(sizecol = "points", sizetype = 'area') %>% 
+    mutate(radius = radius * 0.85)
+
+dat.gg <- circleLayoutVertices(packing, npoints = 100)
+
+labels_cir <- rankings2 %>% 
+    arrange(year) %>% 
+    select(ID, points) %>% 
+    bind_cols(packing)
+
+songs <- rankings2 %>% 
+    mutate(nom_ra = str_c(ID, ". ", title, " - ", artist),
+           axis_x = rep(1:4, each = 79, length = n()),
+           axis_y = rep(79:1, length = n()))
+
+p1 <- 
+ggplot() + 
+    geom_polygon(data = dat.gg, aes(x, y, group = id, fill = factor(id)),
+                 colour = NA) +
+    geom_text(data = labels_cir, aes(x, y, size = points, label = ID),
+              color = "#929299", fontface = "bold",
+              family = "Roboto Condensed Light") +
+    scale_size_continuous(range = c(1, 3)) +
+    scale_fill_manual(values = rankings2$colour) +
+    coord_equal() + 
+    theme_void() +
+    theme(legend.position="none")
+
+
+p2 <- 
+ggplot(songs, aes(axis_x, axis_y)) + 
+    geom_text(aes(label = nom_ra),
+              size = 1.4, hjust = 0,
+              color = "#929299", family = "Roboto Condensed") +
+    xlim(1, 5) +
+    scale_y_continuous(expand = c(0, 1)) +
+    theme_void()
+
+patchwork <- p1 / p2 + plot_layout(heights = c(1, 0.7)) + 
+    plot_annotation(
+    title = "The best hip-hop songs of all time",
+    subtitle = "269 songs by <span style='color:#0b53c1'>**man rappers**</span><br>
+    23 songs by <span style='color:#ff0055'>**women rappers**</span><br>
+    19 songs by <span style='color:#ffec1b'>**mixed bands or collaborations rappers**</span> between female and male artists",
+    caption = "The size of the bubble represents the number of points gained from all songs on the polling list of the BBC\n\nSource: BBC Music \nGraphic: @ysamano28") &
+        theme(plot.background = element_rect(fill = "#252a32", color = "#252a32"),
+              plot.title = element_text(family = "Roboto Condensed", face = "bold",
+                                        size = 30, color = "white",
+                                        hjust = 0.5, margin = margin(t = 15, b = 10)),
+              plot.subtitle = element_markdown(family = "Roboto Condensed",
+                                               size = 13, color = "white", hjust = 0.5,
+                                               margin = margin(b = 0)),
+              plot.caption = element_text(family = "Roboto Condensed",
+                                          size = 6, color = "white", hjust = 0,
+                                          margin = margin(t = 10, b = 5, l = 15)))
+
+
+ggsave("2020/week_16/hip_hop.png", plot = patchwork, height = 12, width = 8, units = "in", dpi = 300)
+
