1281bc1085d3c3ec60a5dc2003d544ecb59df09e
Yobanny Samano <yobanny_sc@hotmail.com>
2020-06-12 23:06:21 -0500

    commit

--- 2020/week_16/week_16_hip_hop.R
+++ 2020/week_16/week_16_hip_hop.R
@@ -1,9 +1,7 @@
-
 library(tidyverse)
 library(packcircles)
 library(patchwork)
 library(ggtext)
-extrafont::loadfonts(device = "win")
 
 rankings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-04-14/rankings.csv')
 
@@ -16,9 +14,9 @@ rankings2 <- rankings %>%
 packing <- rankings2 %>% 
     arrange(year) %>% 
     circleProgressiveLayout(sizecol = "points", sizetype = 'area') %>% 
-    mutate(radius = radius * 0.85)
+    mutate(radius = radius * 0.9)
 
-dat.gg <- circleLayoutVertices(packing, npoints = 100)
+dat.gg <- circleLayoutVertices(packing, npoints = 200)
 
 labels_cir <- rankings2 %>% 
     arrange(year) %>% 
@@ -31,12 +29,15 @@ songs <- rankings2 %>%
            axis_y = rep(79:1, length = n()))
 
 p1 <- 
-ggplot() + 
-    geom_polygon(data = dat.gg, aes(x, y, group = id, fill = factor(id)),
+    ggplot() + 
+    geom_polygon(data = dat.gg,
+                 aes(x, y, group = id, fill = factor(id)),
                  colour = NA) +
-    geom_text(data = labels_cir, aes(x, y, size = points, label = ID),
-              color = "#929299", fontface = "bold",
-              family = "Roboto Condensed Light") +
+    geom_text(data = labels_cir,
+              aes(x, y, size = points, label = ID),
+              color = "#929299",
+              #fontface = "bold",
+              family = "Roboto Condensed") +
     scale_size_continuous(range = c(1, 3)) +
     scale_fill_manual(values = rankings2$colour) +
     coord_equal() + 
@@ -45,32 +46,43 @@ ggplot() +
 
 
 p2 <- 
-ggplot(songs, aes(axis_x, axis_y)) + 
+    ggplot(songs, aes(axis_x, axis_y)) + 
     geom_text(aes(label = nom_ra),
-              size = 1.4, hjust = 0,
-              color = "#929299", family = "Roboto Condensed") +
-    xlim(1, 5) +
-    scale_y_continuous(expand = c(0, 1)) +
+              size = 1.4, 
+              hjust = 0,
+              color = "#929299",
+              family = "Roboto Condensed") +
+    scale_x_continuous(limits = c(0.9, 5), expand = c(0, 0)) +
+    scale_y_continuous(limits = c(0.5, 79.5), expand = c(0, 0)) +
     theme_void()
 
 patchwork <- p1 / p2 + plot_layout(heights = c(1, 0.7)) + 
     plot_annotation(
-    title = "The best hip-hop songs of all time",
-    subtitle = "269 songs by <span style='color:#0b53c1'>**man rappers**</span><br>
+        title = "The best hip-hop songs of all time",
+        subtitle = "269 songs by <span style='color:#0b53c1'>**man rappers**</span><br>
     23 songs by <span style='color:#ff0055'>**women rappers**</span><br>
-    19 songs by <span style='color:#ffec1b'>**mixed bands or collaborations rappers**</span> between female and male artists",
-    caption = "The size of the bubble represents the number of points gained from all songs on the polling list of the BBC\n\nSource: BBC Music \nGraphic: @ysamano28") &
-        theme(plot.background = element_rect(fill = "#252a32", color = "#252a32"),
-              plot.title = element_text(family = "Roboto Condensed", face = "bold",
-                                        size = 30, color = "white",
-                                        hjust = 0.5, margin = margin(t = 15, b = 10)),
-              plot.subtitle = element_markdown(family = "Roboto Condensed",
-                                               size = 13, color = "white", hjust = 0.5,
-                                               margin = margin(b = 0)),
-              plot.caption = element_text(family = "Roboto Condensed",
-                                          size = 6, color = "white", hjust = 0,
-                                          margin = margin(t = 10, b = 5, l = 15)))
+    19 songs by <span style='color:#ffec1b'>**mixed bands or collaborations rappers**</span>",
+        caption = "The size of the bubble represents the number of points gained from all songs on the polling list of the BBC\n
+    Source: BBC Music | Graphic: @ysamano28") &
+    
+    theme(plot.background = element_rect(fill = "#1b1f2b",
+                                         color = "#1b1f2b"),
+          plot.title = element_text(family = "Roboto Condensed",
+                                    face = "bold",
+                                    size = 30, 
+                                    color = "white",
+                                    hjust = 0.5, 
+                                    margin = margin(t = 15, b = 10)),
+          plot.subtitle = element_markdown(family = "Roboto Condensed",
+                                           size = 13, 
+                                           color = "white",
+                                           hjust = 0.5,
+                                           margin = margin(b = 0)),
+          plot.caption = element_text(family = "Roboto Condensed",
+                                      size = 6,
+                                      color = "white",
+                                      hjust = 0,
+                                      margin = margin(t = 10, b = 5, l = 15)))
 
 
-ggsave("2020/week_16/hip_hop.png", plot = patchwork, height = 12, width = 8, units = "in", dpi = 300)
-
+ggsave("2020/week_16/hip_hop_prueba.png", plot = patchwork, height = 11, width = 8.5, units = "in", dpi = 300)
