652ad83686b321e26e4ebb91379bc224c9dc7a15
Yobanny Samano <yobanny_sc@hotmail.com>
2020-06-13 01:18:34 -0500

    commit

--- 2020/week_17/week_17_gdpr_violations.R
+++ 2020/week_17/week_17_gdpr_violations.R
@@ -1,9 +1,8 @@
-
 library(tidyverse)
 
 gdpr_violations <- read_tsv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-04-21/gdpr_violations.tsv')
 
-set.seed(250)
+set.seed(2817)
 
 axis_ <- tibble(axis_x = runif(250),
                  axis_y = runif(250))
@@ -15,8 +14,8 @@ data_graph <- gdpr_violations %>%
                          scales::dollar(price, suffix = "€", prefix = ""), "\n",
                          name))
 
-x11()
-ggplot() +
+p1 <- 
+    ggplot() +
     geom_point(data = data_graph, aes(axis_x, axis_y, size = price), colour = "#39b185", alpha = 0.7) +
     scale_size(range = c(2, 80)) +
     geom_point(data = data_graph, aes(axis_x, axis_y), colour = "#353e4a", size = 1.5) +
@@ -28,17 +27,13 @@ ggplot() +
     labs(title = "The Universe of GDPR Violations",
          subtitle = "Top Companies",
          caption = "Source: Privacy Affairs | Graphic: @ysamano28 ") +
-    theme_void(base_family = "Roboto Condensed") +
+    theme_ybn_b(axis_grid = F, axis_text = F, axis_title = F) +
     theme(legend.position = "none",
-          plot.background = element_rect(fill = "#252a32", color = "#252a32"),
-          plot.title = element_text(face = "bold", size = 30,
-                                    color = "white", hjust = 0.5,
-                                    margin = margin(t = 15, b = 10)),
-          plot.subtitle = element_text(size = 18, color = "white",
-                                       hjust = 0.5,
-                                       margin = margin(t = 0, b = 0)),
-          plot.caption = element_text(size = 10, color = "white",
-                                      margin = margin(b = 10)))
+          plot.title = element_text(size = 30,
+                                    hjust = 0.5),
+          plot.subtitle = element_text(size = 18, 
+                                       hjust = 0.5))
+    
 
-ggsave("2020/week_17/gdpr_violations.png", height = 12, width = 8, units = "in", dpi = 300)
+ggsave("2020/week_17/gdpr_violations.png", p1, height = 11, width = 8.5, units = "in", dpi = 300)
 
