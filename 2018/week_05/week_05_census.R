cb284e9fbe98817fc7babc29ba2b071ff77b4c06
Yobanny Samano <yobanny_sc@hotmail.com>
2020-06-13 22:07:42 -0500

    commit

---
+++ 2018/week_05/week_05_census.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(ggridges)
+
+data <- read_csv("2018/week_05/week5_acs2015_county_data.csv")
+
+p1 <- ggplot(data, aes(IncomePerCap,State)) + 
+  geom_density_ridges(scale = 2,
+                      fill = "#69BEB0",
+                      color = "#69BEB0",
+                      alpha = 0.9) +
+  scale_x_continuous(labels = scales::dollar,
+                     n.breaks = 7) +
+  labs(title = "US Census Demographic 2015",
+       subtitle = "Income Per Capita by State",
+       caption = "US Census Demographic Data 2015 | Graphic: @ysamano28") +
+  theme_ybn_w(base_family = "Roboto Condensed") +
+  theme(axis.title.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5))
+
+
+ggsave("2018/week_05/census.png", p1, height = 11, width = 8.5, units = "in", dpi = 300)
+  
+  
+  
