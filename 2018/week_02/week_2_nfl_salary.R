7c6841209a3d5790bf735801c7bfabc0b004afc5
Yobanny Samano <yobanny_sc@hotmail.com>
2020-10-16 16:18:08 -0500

    tidytuesday 2018 week 2

--- 2018/week_02/week_2_nfl_salary.R
+++ 2018/week_02/week_2_nfl_salary.R
@@ -1,32 +1,23 @@
-
-library(readxl)
 library(tidyverse)
 library(ggbeeswarm)
 
-nfl_salary <- 
-    read_excel("2018/week_02/nfl_salary.xlsx")
+nfl_salary <- readxl::read_excel("2018/week_02/nfl_salary.xlsx")
 
 salary <- nfl_salary %>% 
-    gather(-year, key = "position", value = "valores") %>% 
-    filter(!is.na(valores))
+    pivot_longer(cols = -year,
+                 names_to = "position",
+                 values_to = "salary_position") %>% 
+    filter(!is.na(salary_position))
 
-
-x11()
-ggplot(salary, aes(year, valores / 1000000, group = year)) +
-    geom_quasirandom(size = 0.8, alpha = .3, colour = "#54278f") +
-    facet_wrap(~ position, ncol = 5) +
+p1 <- ggplot(salary, aes(year, salary_position / 1000000, group = year)) +
+    geom_quasirandom(size = 0.7,
+                     alpha = .3,
+                     colour = "#54278f") +
+    facet_wrap( ~ position, ncol = 5) +
     scale_y_continuous(labels = scales::dollar_format(suffix = "m")) +
     labs(x = "Year", y = "Salary",
          title = "NFL Positional Salaries",
-         caption = "Graphic: @ysamano28 | Source: spotrac.com") +
-    theme_minimal(base_size = 8) + 
-    theme(plot.title = element_text(face = "bold", size = 13, colour = "#151B25",
-                                    margin = margin(10, 0, 10, 0)),
-          strip.text.x = element_text(size = 8, face = "bold", margin = margin(5, 0, 5, 0)), 
-          axis.title.x = element_text(hjust = 1, margin = margin(5, 0, 0, 0)),
-          axis.title.y = element_text(hjust = 1, margin = margin(0, 5, 0, 0)))
-          
+         caption = "Source: spotrac.com | Graphic: @ysamano28") +
+    theme_ybn_w(base_size = 7)
 
-ggsave("2018/week_02/nfl_salaries.png", height = 6, width = 10, units = "in", dpi = 400)
-
-
+ggsave("2018/week_02/nfl_salaries.png", p1, height = 6.8, width = 8.8, units = "in", type = "cairo")
